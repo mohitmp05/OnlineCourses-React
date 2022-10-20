fd7b7bc1770d764677b403cc67ad2d2eff94f9f9
Mohit <officialmohit05@gmail.com>
2022-10-20 15:19:01 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App = () => {
+  return(
+    <div className='ui comments'>
+      <div className='comment'>
+        <a href='/' className='avatar'>
+          <img alt='profile picture'></img>
+        </a>
+        <div className='content'>
+          <a href='/' className='author'>Mohit</a>
+          <div className='metadata'>
+            <span className='date'>Today at 7pm</span>
+          </div>
+          <div className='text'>Welcome to React Blog</div>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
